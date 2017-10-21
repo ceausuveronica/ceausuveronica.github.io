1402f734e0ab4fbf2c655dc636b100bf226e1637
ceausuveronica <32710001+ceausuveronica@users.noreply.github.com>
2017-10-21 14:57:08 +0300

    Add files via upload

---
+++ sensors.js
@@ -0,0 +1,10 @@
+window.addEventListener('deviceorientation', ondeviceorientation);
+
+//----------------------------------------
+function ondeviceorientation(event) 
+{
+	document.getElementById("id_alpha").innerHTML = Math.round(event.alpha * 100) / 100;
+	document.getElementById("id_beta").innerHTML = Math.round(event.beta * 100) / 100;
+	document.getElementById("id_gamma").innerHTML = Math.round(event.gamma * 100) / 100;
+}
+//----------------------------------------
