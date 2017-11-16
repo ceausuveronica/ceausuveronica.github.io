2513eca312a1193573fef973abbc72287c77dd42
ceausuveronica <32710001+ceausuveronica@users.noreply.github.com>
2017-11-16 07:51:06 +0200

    15 noiembrie 2017

--- svg_move.js
+++ svg_move.js
@@ -1,4 +1,4 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.0"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.2"; 
 
 var circle1 = document.getElementById("id_circle1");
 var circle2 = document.getElementById("id_circle2");
@@ -12,11 +12,19 @@ var rect_svg = svg.getBoundingClientRect();
 //----------------------------------------
 function on_touch_move(e)
 {
+	e.preventDefault();
+	
 	var touches = e.changedTouches;
 	for (var i = 0; i < touches.length; i++){
 		var circle = touches[i].target;
-		circle.setAttribute("cx", touches[i].pageX - rect_svg.left);
-		circle.setAttribute("cy", touches[i].pageY - rect_svg.top);
+		if (touches[i].pageX - rect_svg.left <= rect_svg.width && 
+			touches[i].pageY - rect_svg.top <= rect_svg.height &&
+			touches[i].pageX - rect_svg.left >= 0 &&
+			touches[i].pageY - rect_svg.top >= 0
+			){
+			circle.setAttribute("cx", touches[i].pageX - rect_svg.left);
+			circle.setAttribute("cy", touches[i].pageY - rect_svg.top);
+		}
 	}
 }
 //----------------------------------------
